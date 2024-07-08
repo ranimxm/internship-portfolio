ff5583d2fd3d29a9e535b80bcf4bf058be9cf2e4
Ranim <i502848@fhict.nl>
2024-07-08 19:03:04 +0200

    setting up preview deployment off part 2

--- ignore-preview-vercel.sh
+++ ignore-preview-vercel.sh
@@ -1,7 +1,11 @@
-if [ "$VERCEL_GIT_COMMIT_REF" != "develop" ]; then
-  echo "Build cancelled"
-  exit 0
-else
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
   echo "Build can proceed"
-  exit 1
+  exit 1;
+else
+  echo "Build cancelled"
+  exit 0;
 fi
