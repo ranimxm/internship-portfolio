3577f8614cb898d59dd2f10f8400b8d6a4048f53
ranimm01 <ranimm01@hotmail.com>
2024-09-21 00:34:50 +0200

    Update IntroView.vue

--- src/components/IntroView.vue
+++ src/components/IntroView.vue
@@ -2,7 +2,7 @@
   <div class="intro">
     <h1 data-text="Hi, I'm Ranim,">Hi, I'm Ranim,</h1>
     <h4>
-      a 22 year old It student currently in my third year at Fontys University.<br />
+      a 22 year old IT student currently in my third year at Fontys University.<br />
       I thrive on challenges and often find myself pulling all-nighters to complete projects,<br />
       with a little help from ChatGPT and Stack Overflow.
     </h4>
